2b1a34cfb7b7f1fef247784ba44d9c62bd178559
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-06-27 09:57:40 +0300

    added tour details

--- src/Components/Tours.jsx
+++ src/Components/Tours.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tour from "./Tour";
 
 const Tours = ({ tours }) => {
   return (
@@ -6,6 +7,11 @@ const Tours = ({ tours }) => {
       <div className="title">
         <h2>our tours</h2>
         <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
       </div>
     </section>
   );
