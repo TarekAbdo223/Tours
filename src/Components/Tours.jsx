a33a00df07d3ff2125ef42dff1d00ec7eb749dc9
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-06-27 09:41:44 +0300

    tours prop

---
+++ src/Components/Tours.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="title-underline"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
